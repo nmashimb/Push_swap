9793e4ed7f1cac0655f0b58b75d2654175a9e3d1
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-07-26 13:16:00 +0200

    ps

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 13:59:45 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/07/24 10:19:45 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/07/26 13:13:58 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,6 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-/*typedef struct		s_list
-{
-//	int				content_size;
-	int				content;
-	struct s_list	*next;
-
-}					t_list;*/
-/*
-t_list				*ft_lstnew(void const *content, size_t content_size);
-void				ft_lstadd(t_list **alst, t_list *new);
-void				ft_lstdelone(t_list **alst, void (del)(void *, size_t));*/
 int					ft_isalpha(int c);
 int					ft_isdigit(int c);
 int					ft_isalnum(int c);
