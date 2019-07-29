3d8fe16d8a367636833743a39be4b13c02104a51
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-07-29 17:21:55 +0200

    ps

--- push_swap.c
+++ push_swap.c
@@ -6,14 +6,12 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/19 14:17:07 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/07/26 12:57:58 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/07/29 17:09:54 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
-//ss(sa and sb at the same time)
-//rrr(rra and rrb at the same time)
 
 void		push_swap(t_list **head)
 {
@@ -27,50 +25,24 @@ void		push_swap(t_list **head)
 	}
 }
 
-int		main()
+int		main(int argc, char **argv)
 {
-	t_list	*head1 = NULL;
-	t_list	*head2 = NULL;
-	t_list	*t;
-	t_list	*s;
+	t_list	*head;
 
-	ft_push(&head1, 1);
-	ft_push(&head1, 2);
-	ft_push(&head1, 3);
-//	ft_push(&head1, 4);
-	//ft_push(&head1, 5);
-	
-	//ft_push(&head2, 9);
-	//ft_push(&head2, 7);
-	//ft_push(&head2, 8);
-	//ft_push(&head2, 6);
+	head = NULL;
+	if (argc == 2)
+		return (1);
+	head = ft_create_a(head, argv, argc);
+	if (head != NULL)
+		push_swap(&head);
 
-	//ft_sa(&head1);
-	//ft_ra(&head);
-	//ft_rra(&head);
 
-	//ft_pa(&head2, &head1);
-	//ft_pa(&head1, &head2);
-	//ft_pa(&head1, &head2);
-	//ft_pa(&head1, &head2);
-	//ft_pa(&head1, &head2);
 
-	//ft_s_below_three(&head1);
-	push_swap(&head1);
-
-	printf("stack A\n");
-	t = head1;
+	t_list *t = head;
 	while (t != NULL)
 	{
-		printf("%d\n", t -> content);
-		t = t -> next;
-	}
-	printf("stack B\n");
-	s = head2;
-	while (s != NULL)
-	{
-		printf("%d\n", s -> content);
-		s = s -> next;
+		printf("%d\n", t->content);
+		t = t->next;
 	}
 	return (0);
 }
