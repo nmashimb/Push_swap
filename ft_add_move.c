2175cb434abc28480f6666fa79f6375d340ab119
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-08-06 15:15:11 +0200

    ps

---
+++ ft_add_move.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_add_move.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/06 15:14:01 by nmashimb          #+#    #+#             */
+/*   Updated: 2019/08/06 15:14:48 by nmashimb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	add_move(mvs_list **mhead, mvs_list **mtl, char *move)
+{
+	mvs_list	*p;
+	mvs_list	*mtail;
+
+	mtail = *mtl;
+	p = (mvs_list *)malloc(sizeof(mvs_list));
+	p->content = move;
+	if (*mhead == NULL)
+	{
+		p->next = NULL;
+		*mhead = p;
+		mtail = p;
+	}
+	else
+	{
+		mtail->next = p;
+		mtail = p;
+		p->next = NULL;
+	}
+	*mtl = mtail;
+	p = NULL;
+}
