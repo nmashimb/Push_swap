85237745658eeaf1b41959d59fdc45a443ae19e8
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-08-06 15:35:13 +0200

    ps

--- checker.c
+++ checker.c
@@ -6,52 +6,17 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/02 13:42:52 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/08/06 15:02:24 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/08/06 15:34:40 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-static	void	add_move(mvs_list **mhead, mvs_list **mtl, char *move)
+/*static void		checker(t_list	*head1, char	**arr_moves)
 {
-	mvs_list	*p;
-	mvs_list	*mtail;
-
-	mtail = *mtl;
-	p = (mvs_list *)malloc(sizeof(mvs_list));
-	p->content = move;
-	if (*mhead == NULL)
-	{
-		p->next = NULL;
-		*mhead = p;
-		mtail = p;
-	}
-	else
-	{
-		mtail->next = p;
-		mtail = p;
-		p->next = NULL;
-	}
-	*mtl = mtail;
-	p = NULL;
-}
-
-/*static void		checker(t_list	*head, char	**arr_moves)
-{
-	sort_game(head, arr_moves);
-}*/
-
-/*static void		arr_moves(char	***line)
-{
-	int	i;
-
-	i = 0;
-	while ((get_next_line(0, *line)) == 1)
-	{
-		printf("lines %s", *line[i]);
-		i++;
-	}
+	t_list	*head2;
+	sorting_game(head, arr_moves);
 }*/
 
 int		main(int argc, char **argv)
