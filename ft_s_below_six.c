e5049f94ddc0912459ca0be29ef4b5372ce7d007
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-08-12 15:02:47 +0200

    ps

---
+++ ft_s_below_six.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_s_below_six.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/12 11:43:01 by nmashimb          #+#    #+#             */
+/*   Updated: 2019/08/12 15:02:16 by nmashimb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include <stdio.h>
+
+void	ft_s_below_six(t_list **hd)
+{
+	t_list	*head;
+	t_list	*head2;
+	t_list	*t;
+	int		count;
+	int		c;
+
+	head = *hd;
+	head2 = NULL;
+	while (ft_stack_len(head) != 3)
+	{
+		ft_pa(&head2, &head);
+		ft_putstr("pb\n");
+	}
+	ft_s_below_three(&head);
+	while (head2 != NULL)
+	{
+		t = head;
+		count = 0;
+		while (ft_peek(head2) > t->content)
+		{
+			count++;
+			t = t->next;
+		}
+		if (count == 1)
+		{
+			ft_pa(&head, &head2);
+			ft_putstr("pa\n");
+			ft_sa(&head);
+			ft_putstr("sa\n");
+		}
+		else if (count >= 2)
+		{
+			c = count - 1;
+			while (c >= 0)
+			{
+				ft_ra(&head);
+				ft_putstr("ra\n");
+				c--;
+			}
+			//could add expr to keep top num of b max
+			ft_pa(&head, &head2);
+			ft_putstr("pa\n");
+			while (c < count - 1)
+			{
+				ft_rra(&head);
+				ft_putstr("rra\n");
+				c++;
+			}
+		}
+		if (count == ft_stack_len(head))
+		{
+			ft_pa(&head, &head2);
+			ft_putstr("pa\n");
+			ft_ra(&head);
+			ft_putstr("ra\n");
+		}
+	}
+
+	t = head;
+	ft_putstr("STACK A\n");
+	while (t != NULL)
+	{
+		printf("%d\n", t->content);
+		t = t->next;
+	}
+	ft_putstr("STACK B\n");
+	t_list *x = head2;
+	while (x != NULL)
+	{
+		printf("%d\n", x->content);
+		x = x->next;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	t_list	*head;
+
+	head = NULL;
+	if (argc == 2)
+		return (1);
+	head = ft_create_a(head, argv, argc);
+	ft_s_below_six(&head);
+	return (0);
+}
