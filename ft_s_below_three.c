7522727487aab9ad7b1b3fb29fc82e06d90d1ae0
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-08-19 11:54:50 +0200

    ps

--- ft_s_below_three.c
+++ ft_s_below_three.c
@@ -6,13 +6,54 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/26 12:53:37 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/08/12 14:40:33 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/08/19 11:53:59 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_s_below_three(t_list **hd)
+static	void	ft_agreater_bc(t_list **hd)
+{
+	t_list	*head;
+
+	head = *hd;
+	if (head->next->content > head->next->next->content)
+	{
+		ft_sa(hd);
+		ft_putstr("sa\n");
+		ft_rra(hd);
+		ft_putstr("rra\n");
+	}
+	else if (head->next->content < head->next->next->content)
+	{
+		ft_rra(hd);
+		ft_putstr("rra\n");
+		ft_rra(hd);
+		ft_putstr("rra\n");
+	}
+}
+
+static	void	ft_aless_bc(t_list **hd)
+{
+	ft_rra(hd);
+	ft_putstr("rra\n");
+	ft_sa(hd);
+	ft_putstr("sa\n");
+}
+
+static	void	ft_sa_two_nu(t_list **hd)
+{
+	ft_sa(hd);
+	ft_putstr("sa\n");
+}
+
+static void		ft_rra_three_nu(t_list **hd)
+{
+	ft_rra(hd);
+	ft_putstr("rra\n");
+}
+
+void			ft_s_below_three(t_list **hd)
 {
 	t_list *head;
 
@@ -20,46 +61,23 @@ void	ft_s_below_three(t_list **hd)
 	if (ft_sort_check(head))
 		return ;
 	if (ft_stack_len(head) == 2)
-	{
-		ft_sa(hd);
-		ft_putstr("sa\n");
-	}
+		ft_sa_two_nu(hd);
 	else if (ft_stack_len(head) == 3)
 	{
-		if (head->content < head->next->content && head->content < head->next->next->content) //a<b a<c 4
-		{
-			ft_rra(hd);
-			ft_putstr("rra\n");
-			ft_sa(hd);
-			ft_putstr("sa\n");
-		}
-		else if (head->content < head->next->content && head->content > head->next->next->content)//a<b a>c 3
-		{
-			ft_rra(hd);
-			ft_putstr("rra\n");
-		}
-		else if (head->content > head->next->content && head->content < head->next->next->content)//a>b a<c 2
+		if (head->content < head->next->content && \
+			head->content < head->next->next->content)
+			ft_aless_bc(hd);
+		else if (head->content < head->next->content &&\
+				head->content > head->next->next->content)
+			ft_rra_three_nu(hd);
+		else if (head->content > head->next->content &&\
+				head->content < head->next->next->content)
 		{
 			ft_sa(hd);
 			ft_putstr("sa\n");
 		}
-		//make a function for this
-		else if (head->content > head->next->content && head->content > head->next->next->content)//a>b a>c 1
-		{
-			if (head->next->content > head->next->next->content) // (a)
-			{
-				ft_sa(hd);
-				ft_putstr("sa\n");
-				ft_rra(hd);
-				ft_putstr("rra\n");
-			}
-			else if (head->next->content < head->next->next->content) // (b)
-			{
-				ft_rra(hd);
-				ft_putstr("rra\n");
-				ft_rra(hd);
-				ft_putstr("rra\n");
-			}
-		}
+		else if (head->content > head->next->content &&\
+				head->content > head->next->next->content)
+			ft_agreater_bc(hd);
 	}
 }
