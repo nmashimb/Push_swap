c0e0d6832ed1c1bc7c2f290de603f94b71708da7
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-09-12 12:50:31 +0200

    ps

--- ft_make_max_third.c
+++ ft_make_max_third.c
@@ -6,7 +6,7 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/02 10:55:12 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/09/10 17:24:36 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/09/12 10:29:16 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,24 @@ static void	ft_while_function(t_list **head, t_list **head2, int min, int max)
 	h = *head;
 	h2 = *head2;
 	ft_while_mins(&h, &h2, min);
-	if (h->content != max && h->content > h->next->content)
+	/*if (h->content != max && h->content > h->next->content)
+	{
+		ft_sa(&h);
+		ft_putstr("sa\n");
+	}*/
+	if (h->next->next->content == max && h->content > h->next->content)
 	{
 		ft_sa(&h);
 		ft_putstr("sa\n");
 	}
-	else if (h->content != max || h->content != min ||\
+	else if (ft_is_sorted_before_max(h) == 0 && (h->content != max || h->content != min ||\
 		h->content != ft_min_two(h, h2) ||\
-		h->content != ft_min_three(h, h2))
+		h->content != ft_min_three(h, h2)))
 	{
 		ft_ra(&h);
 		ft_putstr("ra\n");
 	}
+	//ft_while_mins(&h, &h2, min);
 	*head = h;
 	*head2 = h2;
 }
@@ -56,6 +62,7 @@ void		ft_make_max_third(t_list **head, t_list **head2, int max)
 		ft_sa(&h);
 		ft_putstr("sa\n");
 	}
+	ft_while_mins(&h, &h2, min);
 	*head = h;
 	*head2 = h2;
 }
