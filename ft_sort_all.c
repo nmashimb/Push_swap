4981a1bb310cb856544abd4c760095cef25646c6
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-09-12 12:51:34 +0200

    ps

--- ft_sort_all.c
+++ ft_sort_all.c
@@ -6,7 +6,7 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/27 10:20:31 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/09/10 18:26:48 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/09/12 12:50:49 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ void	ft_sort_all(t_list **hd, t_list **hd2)
 	head2 = *hd2;
 	if (ft_is_sorted_before_max(head) == 0)
 		ft_make_max_third(&head, &head2,ft_find_max(head));
-	while (ft_sort_check(head) == 0)
-		ft_count_to_position(&head, &head2);
+/*	while (ft_sort_check(head) == 0)
+		ft_count_to_position(&head, &head2);*/
 
 
 	//head = *hd;
